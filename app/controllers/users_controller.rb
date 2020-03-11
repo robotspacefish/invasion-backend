b045dc3970780596acee61048b75f92640ddb9c2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-11 13:16:30 -0400

    user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     users = User.all
     render json: UserSerializer.new(users)
   end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: UserSerializer.new(user)
+  end
 end

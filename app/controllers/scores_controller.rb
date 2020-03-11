7f7995974591a865071de2e11c7722c528e0e7de
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-11 13:11:03 -0400

    leaderboard action & route

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,4 +3,16 @@ class ScoresController < ApplicationController
     scores = Score.all
     render json: ScoreSerializer.new(scores)
   end
+
+  def leaderboard
+    high_scores = Score.leaderboard
+    users_and_scores = high_scores.collect do |score|
+      {
+        value: score.value,
+        username: User.find_by(id: score.user_id).username
+      }
+    end
+
+    render json: users_and_scores
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :scores
   resources :users
+  get '/leaderboard', to: 'scores#leaderboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

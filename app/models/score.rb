714e46a3425d8451b0693c624aded2142f235ce5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-11 12:26:52 -0400

    add validations for user and score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,4 @@
 class Score < ApplicationRecord
   belongs_to :user
+  validates :value, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :scores
+  validates :username, presence: true, uniqueness: true
 end

076a4e0532f6a1ef8253b40e9e8bae7dff217e7d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-18 14:54:36 -0400

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,11 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
     render json: UserSerializer.new(user)
   end
+
+  def create
+    user = User.find_or_create_by(username: params[:username])
+    user.scores.build(value: params[:value])
+    user.save
+    render json: UserSerializer.new(user)
+  end
 end

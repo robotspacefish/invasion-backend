088e5f2f77770af9a331c71c2248a54c362b9364
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-06 14:23:53 -0400

    score create action

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,4 +8,10 @@ class ScoresController < ApplicationController
     users_and_scores = Score.leaderboard_users_and_scores
     render json: users_and_scores
   end
+
+  def create
+    score = Score.create(username: params[:username])
+    render json: UserSerializer.new(user)
+  end
+
 end
